9bda1638c68b974a6a30c9b5d6cc8955e8e65f65
minhphan46 <21522345@gm.uit.edu.vn>
2023-04-17 00:01:04 +0700

    customPost

--- lib/modules/search/widgets/result_page/item_product.dart
+++ lib/modules/search/widgets/result_page/item_product.dart
@@ -7,7 +7,7 @@ class ItemProduct extends StatelessWidget {
   String title;
   String size;
   String money;
-  String timeCreated;
+  DateTime timeCreated;
   String location;
   bool isFavourited;
 
@@ -26,94 +26,106 @@ class ItemProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Stack(
-          children: [
-            Row(
-              children: [
-                // image
-                SizedBox(
-                  height: sizeImage,
-                  width: sizeImage,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      urlImage,
-                      fit: BoxFit.cover,
-                    ),
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.only(bottom: 8),
+      color: AppColors.white,
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              // image
+              SizedBox(
+                height: sizeImage,
+                width: sizeImage,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    urlImage,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
                   ),
                 ),
-                const SizedBox(width: 8),
-                // text
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // title
-                      Text(
-                        title,
-                        style: AppTextStyles.roboto14regular,
-                      ),
-                      const SizedBox(height: 5),
-                      // size
-                      Text(
-                        size,
-                        style: AppTextStyles.roboto12regular
-                            .copyWith(color: AppColors.grey),
-                      ),
-                      const SizedBox(height: 5),
-                      // money
-                      Text(
-                        money,
-                        style: AppTextStyles.roboto14semiBold
-                            .copyWith(color: AppColors.red),
-                      ),
-                      const SizedBox(height: 5),
-                      // time + location
-                      Row(
-                        children: [
-                          Text(
-                            timeCreated,
-                            style: AppTextStyles.roboto11regular
-                                .copyWith(color: AppColors.grey),
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            "•",
-                            style: AppTextStyles.roboto11regular
-                                .copyWith(color: AppColors.grey),
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            location,
-                            style: AppTextStyles.roboto11regular
-                                .copyWith(color: AppColors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-
-            // heart
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: SizedBox(
-                child: isFavourited
-                    ? Icon(
-                        Icons.favorite_sharp,
-                        color: AppColors.red,
-                      )
-                    : const Icon(Icons.favorite_border_rounded),
               ),
+              const SizedBox(width: 8),
+              // text
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // title
+                    Text(
+                      title,
+                      style: AppTextStyles.roboto14regular,
+                    ),
+                    const SizedBox(height: 5),
+                    // size
+                    Text(
+                      size,
+                      style: AppTextStyles.roboto12regular
+                          .copyWith(color: AppColors.grey),
+                    ),
+                    const SizedBox(height: 5),
+                    // money
+                    Text(
+                      money,
+                      style: AppTextStyles.roboto14semiBold
+                          .copyWith(color: AppColors.red),
+                    ),
+                    const SizedBox(height: 5),
+                    // time + location
+                    Row(
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.roboto11regular
+                              .copyWith(color: AppColors.grey),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          "•",
+                          style: AppTextStyles.roboto11regular
+                              .copyWith(color: AppColors.grey),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          location,
+                          style: AppTextStyles.roboto11regular
+                              .copyWith(color: AppColors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // heart
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              child: isFavourited
+                  ? Icon(
+                      Icons.favorite_sharp,
+                      color: AppColors.red,
+                    )
+                  : const Icon(Icons.favorite_border_rounded),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/modules/search/widgets/result_page/related_list.dart
+++ lib/modules/search/widgets/result_page/related_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nha_gia_re/data/models/post.dart';
 import 'package:nha_gia_re/data/models/product.dart';
 import 'package:nha_gia_re/data/providers/data_center.dart';
 import 'package:nha_gia_re/modules/search/widgets/result_page/item_product.dart';
@@ -12,17 +13,17 @@ class RelatedList extends StatelessWidget {
     return Container(
       color: AppColors.backgroundColor,
       child: ListView.builder(
-        itemCount: DataCenter.instance.products.length,
+        itemCount: DataCenter.instance.dummyPosts.length,
         itemBuilder: (_, i) {
-          Product prod = DataCenter.instance.products[i];
+          Post prod = DataCenter.instance.dummyPosts[i];
           return ItemProduct(
-            urlImage: prod.urlImage!,
+            urlImage: prod.imagesUrl[0],
             title: prod.title,
-            money: prod.money,
-            size: prod.size,
-            location: prod.location,
-            timeCreated: prod.timeCreated!,
-            isFavourited: prod.isFavourited!,
+            money: prod.price.toString(),
+            size: "100m2",
+            location: "Long An",
+            timeCreated: prod.postedAt,
+            isFavourited: false,
           );
         },
       ),
