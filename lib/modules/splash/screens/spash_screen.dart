b0fedf3ab0ecb12dd5ab9cda5f744045db90895b
nhao2003 <nhathaodev@gmail.com>
2023-07-06 11:40:48 +0700

    add animation splash_screen

--- lib/modules/splash/screens/spash_screen.dart
+++ lib/modules/splash/screens/spash_screen.dart
@@ -1,28 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get_it/get_it.dart';
 import 'package:nha_gia_re/core/theme/app_colors.dart';
 import 'package:nha_gia_re/core/theme/text_styles.dart';
 import 'package:nha_gia_re/modules/splash/splash_controller.dart';
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+import 'dart:async';
+import 'package:animations/animations.dart';
 
+import '../../../core/values/assets_image.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../../routers/app_routes.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//
+//
+//   @override
+//   void initState() {
+//     _controller.init();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColors.primaryColor,
+//       body: Center(
+//         child: Text(
+//           "Nhà Giá Rẻ",
+//           style: AppTextStyles.roboto20semiBold,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  final SplashController _controller = SplashController();
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  double _fontSize = 2;
+  double _containerSize = 1.5;
+  double _textOpacity = 0.0;
+  double _containerOpacity = 0.0;
+
+  late final AnimationController _controller;
+  late final Animation<double> animation1;
+  final SplashController splashController = SplashController();
   @override
   void initState() {
     super.initState();
-    _controller.init();
+
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    _controller.addListener(() {
+      if(_controller.isCompleted){
+        Future.delayed(const Duration(milliseconds: 500));
+        splashController.init();
+      }
+    });
+
+    animation1 = Tween<double>(begin: 40, end: 20).animate(CurvedAnimation(
+        parent: _controller, curve: Curves.fastLinearToSlowEaseIn))
+      ..addListener(() {
+        setState(() {
+          _textOpacity = 1.0;
+        });
+      });
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 2), () {
+      setState(() {
+        _fontSize = 1.06;
+      });
+    });
+
+    Timer(const Duration(seconds: 2), () {
+      setState(() {
+        _containerSize = 2;
+        _containerOpacity = 1;
+      });
+    });
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body:  Center(
-        child: Text("Nhà Giá Rẻ", style: AppTextStyles.roboto20semiBold,),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              AnimatedContainer(
+                  duration: Duration(milliseconds: 2000),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  height: height / _fontSize
+              ),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 1000),
+                opacity: _textOpacity,
+                child: Text(
+                  'YOUR APP\'S NAME',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: animation1.value,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 2000),
+              curve: Curves.fastLinearToSlowEaseIn,
+              opacity: _containerOpacity,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 2000),
+                curve: Curves.fastLinearToSlowEaseIn,
+                height: width / _containerSize,
+                width: width / _containerSize,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                // child: Image.asset('assets/images/file_name.png')
+                child: Image.asset(Assets.coin),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class PageTransition extends PageRouteBuilder {
+  final Widget page;
+
+  PageTransition(this.page)
+      : super(
+    pageBuilder: (context, animation, anotherAnimation) => page,
+    transitionDuration: const Duration(milliseconds: 2000),
+    transitionsBuilder: (context, animation, anotherAnimation, child) {
+      animation = CurvedAnimation(
+        curve: Curves.fastLinearToSlowEaseIn,
+        parent: animation,
+      );
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: SizeTransition(
+          sizeFactor: animation,
+          axisAlignment: 0,
+          child: page,
+        ),
+      );
+    },
+  );
 }
